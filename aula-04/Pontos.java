3379e34352b64a4c55c65d1a322b1749d2e23981
jennigoberski <101295467+jennigoberski@users.noreply.github.com>
2022-04-06 12:03:54 -0300

    Adding mew class

---
+++ aula-04/Pontos.java
@@ -0,0 +1,8 @@
+public class Pontos {
+    /*
+    distância entre dois pontos: 
+    d = Raia quadrada de de (x1-x0)^2 +  (y1-y0)^2
+
+    p2.distancia() -> calcula a distancia de p2 até a origem
+    */ 
+}
